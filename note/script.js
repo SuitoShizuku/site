1e152cfbb878429f2f11fc59b5459f4fb3b12fd3
suitoshizuku <shizuku.suito@gmail.com>
2025-09-27 11:41:25 +0900

    fix

--- note/script.js
+++ note/script.js
@@ -1,4 +1,14 @@
 window.addEventListener('load', function () {
   document.cookie = "DaitoJavascript=On;";
+  document.cookie = "DATA=TRUE";
     console.log(document.cookie)
+
+    let cookie = document.cookie
+    const Data = cookie.split(';')
+    const Root = document.getElementById('root')
+    for(let i=0;i<Data.length;i++){
+        let d = document.createElement('p')
+        d.textContent = Data[i]
+        Root.appendChild(d)
+    }
 });
